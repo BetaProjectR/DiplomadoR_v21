c1e64ed7ef68e372d0def460246d643c7b2fadb0
Angelica <tesisdoctoralar@gmail.com>
2021-04-26 21:18:17 -0300

    Borré este archivo.

--- Transformar formato.R
+++
@@ -1,8 +0,0 @@
-library("remotes")
-remotes::install_github("datalorax/slidex")
-devtools::install_github("datalorax/slidex")
-library(devtools)
-library(slidex)
-pptx <- system.file("~/GitHub/DiplomadoR_Acuicultura_v21/Clase_2/", "CLASE_2.pptx", package = "slidex")
-
-convert_pptx(path = pptx, author = "jose gallardo")
